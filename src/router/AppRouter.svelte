cc05859b03241719e84f680d62b559c910f5f39e
Bün Colak <bun.colak@motius.de>
2021-09-02 10:56:33 +0300

    router

---
+++ src/router/AppRouter.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {Router, Route} from "svelte-navigator";
+    import Login from "../pages/Login.svelte";
+    import Register from "../pages/Register.svelte";
+
+</script>
+
+<Router>
+    <Route path="/register" component={Register} />
+    <Route path="/login" component={Login} />
+    <Route path="*" component={Login} />
+</Router>
