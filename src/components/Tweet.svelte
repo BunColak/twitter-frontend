810d61768a281c19c3e2c09a9c766619b2bf2bde
Bün Colak <bun.colak@motius.de>
2021-09-03 16:03:58 +0300

    like dislike

--- src/components/Tweet.svelte
+++ src/components/Tweet.svelte
@@ -1,8 +1,28 @@
 <script>
     import {formatDistanceToNow} from "date-fns";
     import {Link} from "svelte-navigator";
+    import LikeMutation from '../graphql/LikeMutation.graphql'
+    import DislikeMutation from '../graphql/DislikeMutation.graphql'
+    import {mutation} from "@urql/svelte";
+    const like = mutation({query: LikeMutation})
+    const dislike = mutation({query: DislikeMutation})
 
     export let tweet
+
+    let likedState = tweet.is_liked_by_user
+    let likedCount = tweet.likes_aggregate.aggregate.count
+
+    const likeToggle = async () => {
+        if (likedState) {
+            likedState = !likedState
+            likedCount--
+            await dislike({tweetId: tweet.id})
+        } else {
+            likedState = !likedState
+            likedCount++
+            await like({tweetId: tweet.id})
+        }
+    }
 </script>
 
 <div class="box is-shadowless">
@@ -13,4 +33,10 @@
             ago</span>
     </div>
     <p class="mt-2 has-text-black">{tweet.content}</p>
+    <div on:click={likeToggle} class="block">
+        <span class="icon">
+            <i class:fas={likedState} class:far={!likedState} class="fa-heart has-text-danger"></i>
+        </span>
+        <span>{likedCount || ""}</span>
+    </div>
 </div>
